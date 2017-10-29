827a40e08f453cbb5e26db526fb884102f176a79
cwc <coleman@owt.com>
2017-10-29 10:28:09 -0700

    added all

---
+++ cisco/Cisco IOS Router Basic Configuration _ NetworkLessons.com_files/actionbox-helper.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){var $_magicactionbox=jQuery('.magic-action-box');$_magicactionbox.find('.mab-main-copy p:last-child').addClass('mablast');$_magicactionbox.find('.mab-secondary-copy p:last-child').addClass('mablast');$_magicactionbox.find('.mab-main-action-wrap form .mab-field:last').addClass('mablast');});
---
+++ cisco/Cisco IOS Router Basic Configuration _ NetworkLessons.com_files/global.js
@@ -0,0 +1,1 @@
+var isAdministrationSection=MemberMouseGlobal.jsIsAdmin;var $=jQuery;var wpadmin_url=MemberMouseGlobal.adminUrl;var globalurl=MemberMouseGlobal.globalurl;
